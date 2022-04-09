b8d136bafafe597b351b55fb093cdec3790d5640
resedat <barlinsedat@gmail.com>
2022-04-09 13:05:40 -0700

    Image viewer app

---
+++ Project 1/DetailViewController.swift
@@ -0,0 +1,43 @@
+//
+//  DetailViewController.swift
+//  project1
+//
+//  Created by resedat on 26.03.2022.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+    var selectedImage: String?
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = selectedImage
+        navigationItem.largeTitleDisplayMode = .never
+
+        if let imageToLoad = selectedImage {
+            imageView.image = UIImage(named: imageToLoad)
+        }
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Project 1/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  project1
+//
+//  Created by resedat on 26.03.2022.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    var pictures = [String]()
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Storm Viewer"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("nssl") {
+                pictures.append(item)
+            }
+            
+        }
+        print(pictures)
+
+}
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
+    }
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+            vc.selectedImage = pictures[indexPath.row]
+            navigationController?.pushViewController(vc, animated: true)
+        }
+    }
+    
+
+}
