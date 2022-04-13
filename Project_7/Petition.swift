97b8390ead2b896d0ee523ecff7570af934b4471
resedat <barlinsedat@gmail.com>
2022-04-13 13:21:56 -0700

    JSON-UITabBar-WebKit

---
+++ Project_7/Petition.swift
@@ -0,0 +1,14 @@
+//
+//  Petition.swift
+//  Project_7
+//
+//  Created by resedat on 11.04.2022.
+//
+
+import Foundation
+
+struct Petition: Codable {
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
---
+++ Project_7/Petitions.swift
@@ -0,0 +1,12 @@
+//
+//  Petitions.swift
+//  Project_7
+//
+//  Created by resedat on 11.04.2022.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    var results: [Petition]
+}
